01d1ddbb9588b00c76da6ea8147c43e40a786493
wxq <2418222923@qq.com>
2021-06-24 17:09:25 +0800

    创建账单表

---
+++ database/models/bill/bill.go
@@ -0,0 +1,13 @@
+package bill
+
+import "time"
+
+// Bill 账单表
+type Bill struct {
+	ID           uint64 `gorm:"primary_key;auto_increment"`
+	UserID       uint64
+	Receipt      int       `gorm:"type:int;default:0"`
+	Disbursement int       `gorm:"type:int;default:0"`
+	Type         string    `gorm:"type:varchar(25);default:''"`
+	Time         time.Time `gorm:"type:datetime;default:'1000-01-01 00:00:00'"`
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"FamilyMoneyRecord/database"
+	bill2 "FamilyMoneyRecord/database/models/bill"
 	user2 "FamilyMoneyRecord/database/models/user"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -16,7 +17,7 @@ func main() {
 		fmt.Println("初始化数据库失败，请检查原因重新启动程序")
 		return
 	}
-	err = db.AutoMigrate(&user2.User{})
+	err = db.AutoMigrate(&user2.User{}, &bill2.Bill{})
 	if err != nil {
 		fmt.Println("数据库动态迁移失败，请检查原因重新启动程序")
 	}
