39c88dca3c320320168c51a60cf9942f5bda8b69
wxq <2418222923@qq.com>
2021-06-27 10:33:04 +0800

    为证券账户表操作添加测试

---
+++ database/action/account/account_test.go
@@ -0,0 +1,41 @@
+package account
+
+import (
+	"FamilyMoneyRecord/database"
+	"fmt"
+	"testing"
+)
+
+func TestAddAccount(t *testing.T) {
+	db, _ := database.InitDB()
+	err := AddAccount(db, 1)
+	if err != nil {
+		t.Errorf("添加证券账户错误：%s", err)
+	}
+}
+
+func TestUpdateAccountProfit(t *testing.T) {
+	db, _ := database.InitDB()
+	account, err := UpdateAccountProfit(db, 2, 20)
+	if err != nil {
+		t.Errorf("修改盈利出错：%s", err)
+	}
+	fmt.Println(account)
+}
+
+func TestGetAccountsByUserID(t *testing.T) {
+	db, _ := database.InitDB()
+	accounts, err := GetAccountsByUserID(db, 1)
+	if err != nil {
+		t.Errorf("获取证券账户列表出错：%s", err)
+	}
+	fmt.Println(accounts)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	db, _ := database.InitDB()
+	err := DeleteAccount(db, 3)
+	if err != nil {
+		t.Errorf("删除证券账户失败：%s", err)
+	}
+}
