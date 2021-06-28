21992a71705947b368fff32d1ba706203955f5c6
wxq <2418222923@qq.com>
2021-06-28 10:53:41 +0800

    添加密码加密算法

---
+++ utils/encrypt_util.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+)
+
+// Encrypt 密码加密算法
+func Encrypt(data string) (string, error) {
+	if data == "" {
+		return "", errors.New("密码为空")
+	}
+
+	h := md5.New()
+
+	// 加yan操作
+	dataBytes := []byte(data)
+	if dataBytes[0] > 3 {
+		dataBytes[0] -= 2
+	} else {
+		dataBytes[0]++
+	}
+
+	h.Write(dataBytes)
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
