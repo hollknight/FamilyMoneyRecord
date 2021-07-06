8ad4ad27adf076c3d0e4b9742492580bf80d9922
wxq <2418222923@qq.com>
2021-07-06 09:51:43 +0800

    添加删除错有表和初始化所有表函数的测试函数

---
+++ utils/database_utils/database_utils_test.go
@@ -0,0 +1,20 @@
+package database_utils
+
+import (
+	"FamilyMoneyRecord/database"
+	"testing"
+)
+
+func TestDropAllTables(t *testing.T) {
+	db, _ := database.InitDB()
+	DropAllTables(db)
+}
+
+func TestCreateTables(t *testing.T) {
+	db, _ := database.InitDB()
+	DropAllTables(db)
+	err := CreateTables(db)
+	if err != nil {
+		t.Errorf("创建表单时发生错误：%s", err)
+	}
+}
