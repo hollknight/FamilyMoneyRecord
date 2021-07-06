7aa0f53095bbd15e15d16d0a65f8a7c129d0ac55
wxq <2418222923@qq.com>
2021-07-06 09:53:01 +0800

    添加获取股票实时信息函数的测试函数

---
+++ utils/stock_info_utils/get_stock_info_test.go
@@ -0,0 +1,15 @@
+package stock_info_utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetStockInfo(t *testing.T) {
+	name, price, err := GetStockInfo("sz002307")
+	if err != nil {
+		t.Errorf("获取股票信息时发生错误：%s", err)
+	}
+	fmt.Println(name)
+	fmt.Println(price)
+}
